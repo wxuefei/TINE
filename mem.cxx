3e2f1bc309745ff8e9ee72eab8525c1d1eed260f
Me <enzymical@duck.com>
2023-07-02 16:06:11 +0000

    mem

--- mem.cxx
+++ mem.cxx
@@ -26,22 +26,6 @@ using std::ios;
 #include <winnt.h>
 #endif
 
-// from Glossary.DD
-/*
- * TempleOS uses the asm CALL inst, exclusively, and that inst is limited to
- * calling routines +/-2Gig from the current code location. To prevent
- * out-of-range issues, I decided to separate code and data, placing all code
- * within the lowest 2Gig of memory, addresses 00000000-7FFFFFFF. The compiler
- * and Load()er alloc memory from the code heap to store code and glbl vars,
- * unless the compiler option "OPTf_GLBLS_ON_DATA_HEAP" is used. When programs
- * call MAlloc() is from the data heap, which in not limited in size, except by
- * physical RAM memory. You can alloc from any heap in any task at any time on
- * any core, even making independent (with MemPagAlloc) heaps.
- */
-enum : uint32_t {
-  MAX_CODE_HEAP_ADDR = 0x7fffFFFF
-};
-
 static inline uint64_t Hex2U64(char const* ptr, char const** res) {
   uint64_t ret = 0;
   char c;
@@ -50,8 +34,7 @@ static inline uint64_t Hex2U64(char const* ptr, char const** res) {
     ret |= isalpha(c) ? toupper(c) - 'A' + 10 : c - '0';
     ++ptr;
   }
-  if (res)
-    *res = ptr;
+  *res = ptr;
   return ret;
 }
 
